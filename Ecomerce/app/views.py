5bd931082546d06489c804f2da077352c3fda7a7
loukiddo <138726398+loukiddo@users.noreply.github.com>
2025-10-18 00:00:21 -0300

    Refactor views.py for improved readability

--- Ecomerce/app/views.py
+++ Ecomerce/app/views.py
@@ -3,12 +3,12 @@ from django.db.models import Count, Min
 from .models import Pelicula
 
 def main(request):
-    genero_activo = request.GET.get('genero')  # filtro desde la navbar
+    genero_activo = request.GET.get('genero')  
     peliculas = Pelicula.objects.all()
     if genero_activo:
         peliculas = peliculas.filter(genero=genero_activo)
 
-    # por si quieres usar la lista en otros lugares
+    
     generos = (Pelicula.objects.order_by()
                .values_list('genero', flat=True)
                .distinct())
@@ -27,5 +27,6 @@ def detalles_pelicula(request, id):
 def genero(request, genero):
     peliculas = Pelicula.objects.filter(genero=genero)
     return render(request, 'genero.html', {'peliculas': peliculas, 'genero': genero})
+
 
 
